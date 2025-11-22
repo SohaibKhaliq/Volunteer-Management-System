31613811c1ff6511550cedbc3d9f0bd9ccbcf072
SohaibKhaliq <sohaibkhaliq510@gmail.com>
2025-11-22 17:47:54 +0500

    Initial Commit

---
+++ src/components/AppLogo.jsx
@@ -0,0 +1,17 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import logoDark from '@/assets/images/logo-black.png';
+import logo from '@/assets/images/logo.png';
+const AppLogo = ({
+  height
+}) => {
+  return <>
+      <Link href="/" className="logo-dark">
+        <Image src={logoDark} alt="dark logo" height={height ?? 28} />
+      </Link>
+      <Link href="/" className="logo-light">
+        <Image src={logo} alt="logo" height={height ?? 28} />
+      </Link>
+    </>;
+};
+export default AppLogo;
